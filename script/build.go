d9d3cbeaa0151e604b62fda095a83faf2af043a6
Sanemat <o.gata.ken@gmail.com>
2017-04-23 00:12:52 +0900

    chore(app): build script

--- script/build.go
+++ script/build.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"log"
+	"os/exec"
+)
 
+func goGetGox() {
+	goPath, err := exec.LookPath("go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd := exec.Command(goPath, "get", "github.com/mitchellh/gox")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func goxRun() {
+	goxPath, err := exec.LookPath("gox")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd := exec.Command(
+		goxPath,
+		"-output",
+		"pkg/{{.OS}}_{{.Arch}}/{{.Dir}}",
+		"./cmd/gup",
+	)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	goGetGox()
+	goxRun()
 }
