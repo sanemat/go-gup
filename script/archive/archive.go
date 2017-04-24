da2f0dfbe092e0fc3a2cd495a38eb1bd8214576c
Sanemat <o.gata.ken@gmail.com>
2017-04-25 01:21:34 +0900

    chore(archive): Add archive

--- script/archive/archive.go
+++ script/archive/archive.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
 
+	"github.com/mholt/archiver"
+	"github.com/sanemat/go-gup/script/gitdescribetags"
+)
+
+func main() {
+	tag, err := gitdescribetags.Get()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dirPath := filepath.Join("pkg", tag)
+	fileinfos, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, info := range fileinfos {
+		if !info.IsDir() {
+			continue
+		}
+		if err := archiver.Zip.Make(filepath.Join(dirPath, info.Name()+".zip"), []string{filepath.Join(dirPath, info.Name())}); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
