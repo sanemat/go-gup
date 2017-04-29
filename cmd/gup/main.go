f01b59019b0b38d21dfa9875609577a409a6a1d8
Sanemat <o.gata.ken@gmail.com>
2017-04-29 22:19:31 +0900

    chore(main): remove unnecessary comment

--- cmd/gup/main.go
+++ cmd/gup/main.go
@@ -14,14 +14,8 @@ import (
 var version = "v0.1.3"
 
 func init() {
-	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
 	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
 	log.SetLevel(log.DebugLevel)
 }
 
